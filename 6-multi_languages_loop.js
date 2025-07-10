694cc6035aecf21868589c14d2244227cdf39358
kashikoi2 <hakim.belman@gmail.com>
2025-07-10 18:50:13 +0100

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,9 @@
-let languages=['C is fun','Puthon is cool','JavaScript is amazing'];
-for(let i=0 ; i<languages.length;i++){
-    console.log(languages[i])
+const messages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+for (const message of messages) {
+  console.log(message);
 }
