21e3d23b787635633a77cb0af6dd797c3f193983
kashikoi2 <hakim.belman@gmail.com>
2025-07-10 18:29:56 +0100

    Add files via upload

---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+// Use const to get the arguments
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Print in the specified format
+console.log(firstArg + " is " + secondArg);
